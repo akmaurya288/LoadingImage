ec567876a783d4100b0dcaa4a3e4f6284ccb4e19
akmaurya288 <akmaurya288@gmail.com>
2019-10-15 06:20:46 +0530

    Code to Load and Display Image

--- app/src/main/java/com/akashmaurya/loadingimage/MainActivity.java
+++ app/src/main/java/com/akashmaurya/loadingimage/MainActivity.java
@@ -1,13 +1,20 @@
 package com.akashmaurya.loadingimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,18 +29,47 @@ public class MainActivity extends AppCompatActivity {
 
         netImageWithGlide();
         netImageWithFresco();
+
+        new showImageFromNet((ImageView) findViewById(R.id.imageView_Code))
+                .execute("https://www.gstatic.com/webp/gallery/4.jpg");
     }
 
     public void netImageWithGlide(){
         Glide.with(this)
-                .load("https://moodle.htwchur.ch/pluginfile.php/124614/mod_page/content/4/example.jpg")
+                .load("https://www.gstatic.com/webp/gallery/3.webp")
                 .override(800,600)
                 .into(imageView);
     }
 
     public void netImageWithFresco(){
-        Uri imageUri = Uri.parse("https://i.imgur.com/tGbaZCY.jpg");
+        Uri imageUri = Uri.parse("https://www.gstatic.com/webp/gallery/1.webp");
         SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.sdvImage);
         draweeView.setImageURI(imageUri);
     }
+
+    private class showImageFromNet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public showImageFromNet(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
+
 }
